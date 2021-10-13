e90948d3ba5ec9f2688409eb6d1504e3064736bc
Chiara <58979857+chia030@users.noreply.github.com>
2021-10-13 10:48:30 +0200

    Delete Cinema-project directory

--- Cinema-project/src/main/java/com/example/cinemaproject/repositories/CinemaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.cinemaproject.repositories;
-
-import com.example.cinemaproject.entities.Cinema;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CinemaRepository extends CrudRepository<Cinema, Integer> {
-}
--- Cinema-project/src/main/java/com/example/cinemaproject/repositories/MovieRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.cinemaproject.repositories;
-
-import com.example.cinemaproject.entities.Movie;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MovieRepository extends CrudRepository<Movie, Integer> {
-}
