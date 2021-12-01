e63f602a8ac7bdd05d82d14958d7174b8e73e2e0
chia-fec <chiaravisca@flexecharge.com>
2021-12-01 12:11:02 +0100

    Added WebConfig, needs some fixes still

---
+++ src/main/java/com/kea/cinemaxx/configuration/WebConfig.java
@@ -0,0 +1,26 @@
+package com.kea.cinemaxx.configuration;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:9090",
+                        "http://127.0.0.1:9090",
+                        "https://flawless-branch.surge.sh")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowCredentials(true)
+                .maxAge(3600); //1 hour
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+}
